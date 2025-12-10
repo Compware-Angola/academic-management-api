9ecf78f237f3e524aa9d915bb05c1a969334a3bc
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2025-12-10 13:04:35 +0100

    feat:update data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => {
+    const isSSL = config.get<string>('DB_SSL') === 'true';
+
+    return {
+      type: 'oracle' as const,
+      host: config.get<string>('DB_HOST'),
+      port: config.get<number>('DB_PORT', 1521),
+      username: config.get<string>('DB_USERNAME'),
+      password: config.get<string>('DB_PASSWORD'),
+      sid: config.get<string>('DB_SID'),
+
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      logging: ['query', 'error'],
+
+      extra: {
+        disableInsertDefaultValues: true,
+        ...(isSSL ? { ssl: { rejectUnauthorized: true } } : {}),
+      },
+    };
+  },
+}),],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ✅ Permitir todas as origens CORS
+  app.enableCors({
+    origin: '*', // permite todas as origens
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  // Prefixo global
+  app.setGlobalPrefix('api');
+
+  // === INÍCIO DA CONFIGURAÇÃO DO SWAGGER ===
+  const config = new DocumentBuilder()
+    .setTitle('Academic Service API')
+    .setDescription(
+      'Serviço responsável Pela Gestáo Academica.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  // === FIM DA CONFIGURAÇÃO DO SWAGGER ===
+
+  // Validação global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Porta
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`🚀 API Listening on port ${port}.`);
+  console.log(`📖 Swagger Docs available at ${await app.getUrl()}/api/docs`);
 }
 bootstrap();
+
