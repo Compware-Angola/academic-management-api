898fd3b3b9c5c8fee1566f7cbf608ea5da971617
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2025-12-26 09:54:29 +0100

    feat:update data

--- src/modules/assessment/dto/list-general-agenda.dto.ts
+++ src/modules/assessment/dto/list-general-agenda.dto.ts
@@ -6,7 +6,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class GeneralAgendaDto {
   @ApiProperty({
     description: 'Ano letivo no formato de dois dígitos (ex: 25 para 2025/2026)',
-    example: 25,
+    example: 21,
     minimum: 0,
     type: Number,
   })
@@ -17,8 +17,9 @@ export class GeneralAgendaDto {
 
   @ApiProperty({
     description: 'Código da Grade Curricular (ID da unidade curricular na grade)',
-    example: 42,
+    example: 714,
     type: Number,
+     required:false
   })
   @IsInt()
   @Type(() => Number)
@@ -26,8 +27,9 @@ export class GeneralAgendaDto {
 
   @ApiProperty({
     description: 'Código do horário associado à turma/disciplina',
-    example: 2,
+    example: 20505,
     type: Number,
+     required:false
   })
   @IsInt()
   @Type(() => Number)
@@ -35,7 +37,7 @@ export class GeneralAgendaDto {
 
   @ApiProperty({
     description: 'Semestre da disciplina',
-    example: 1,
+    example: 2,
     enum: [1, 2],
     type: Number,
   })
@@ -48,6 +50,7 @@ export class GeneralAgendaDto {
     description: 'Código da turma',
     example: 8,
     type: Number,
+    required:false
   })
   @IsInt()
   @Type(() => Number)
@@ -57,6 +60,7 @@ export class GeneralAgendaDto {
     description: 'Código da grade curricular específica da turma (gradeCurricularTurma)',
     example: 15,
     type: Number,
+     required:false
   })
   @IsInt()
   @Type(() => Number)
