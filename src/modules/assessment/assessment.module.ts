7a3ca83d68affea399e25f1376d960b46647b349
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2025-12-29 09:08:20 +0100

    feat:update data

--- src/modules/assessment/assessment.module.ts
+++ src/modules/assessment/assessment.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AssessmentService } from './assessment.service';
 import { AssessmentController } from './assessment.controller';
 import { AnoLectivoUtil } from '../util/current-academic-year';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DefineFormulaUcService } from './define_formula_uc.service';
 import { DefineFormulaUcOralService } from './define_formula_uc_oral.service';
