f73bd577e36efcd76f74f07a79f7484b7991a8d0
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-01-26 21:41:33 +0100

    feat: enhance password update validation with complexity requirements

--- src/modules/acess_management/dto/update-password.dto.ts
+++ src/modules/acess_management/dto/update-password.dto.ts
@@ -1,5 +1,5 @@
-// src/users/dto/update-password.dto.ts
-import { IsNotEmpty, IsString, MinLength, IsInt } from 'class-validator';
+
+import { IsNotEmpty, IsString, MinLength, IsInt, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePasswordDto {
@@ -8,9 +8,16 @@ export class UpdatePasswordDto {
   @IsNotEmpty()
   utilizadorId: number;
 
-  @ApiProperty({ example: 'novaSenha@2025', minLength: 8, description: 'Nova senha' })
+  @ApiProperty({ 
+    example: 'NovaSenha@2025', 
+    minLength: 8, 
+    description: 'Nova senha (mínimo 8 caracteres, 1 maiúscula, 1 minúscula, 1 número e 1 símbolo)' 
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8, { message: 'A senha deve ter no mínimo 8 caracteres' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{}|;:',.<>/?~])[A-Za-z\d!@#$%^&*()_+\-=[\]{}|;:',.<>/?~]+$/, {
+    message: 'A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um símbolo',
+  })
   novaSenha: string;
 }
