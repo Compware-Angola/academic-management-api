af5d96916bf6335f3b4e1d583bd50708c060a8c4
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-02-12 18:37:57 +0100

    feat:update data

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,9 +20,18 @@ import { EnrollmentModule } from './modules/enrollment/enrollment.module';
 
 @Module({
   imports: [
-   ConfigModule.forRoot({
+       ConfigModule.forRoot({
       isGlobal: true,
-       envFilePath: process.env.ENV_FILE || '.env.dev',
+      envFilePath: (() => {
+        switch (process.env.NODE_ENV) {
+          case 'production':
+            return '.env.prod';       
+          case 'preprod':
+            return '.env.preprod';    
+          default:
+            return '.env.dev';        
+        }
+      })(),
     }),
     HttpModule,
     TypeOrmModule.forRootAsync({
