74660f62fc41672e925cea8928c1088ebde38727
Domingos Canhanga <canhanga96@gmail.com>
2026-02-23 12:39:03 +0100

    feat: remove defense management TFC module

--- src/modules/defense-management-tfc/defense-management-tfc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DefenseManagementTfcService } from './defense-management-tfc.service';
-import { DefenseManagementTfcController } from './defense-management-tfc.controller';
-
-@Module({
-  controllers: [DefenseManagementTfcController],
-  providers: [DefenseManagementTfcService],
-})
-export class DefenseManagementTfcModule {}
