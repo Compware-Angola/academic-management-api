d21701c7cd810b5c485d73bfc9c0ba494a57a01b
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-03-11 12:45:10 +0100

    feat: implement getTeacherParameters and toggleTeacherParameter methods with pagination and search functionality

--- src/modules/docente_gestao/docente_gestao.controller.ts
+++ src/modules/docente_gestao/docente_gestao.controller.ts
@@ -1,34 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { DocenteGestaoService } from './docente_gestao.service';
 import { CreateDocenteGestaoDto } from './dto/create-docente_gestao.dto';
 import { UpdateDocenteGestaoDto } from './dto/update-docente_gestao.dto';
+import { FindParametrosDocenteTO } from './dto/find-parametros-docente.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('docente-gestao')
 export class DocenteGestaoController {
-  constructor(private readonly docenteGestaoService: DocenteGestaoService) {}
-
-  @Post()
-  create(@Body() createDocenteGestaoDto: CreateDocenteGestaoDto) {
-    return this.docenteGestaoService.create(createDocenteGestaoDto);
+  constructor(private readonly service: DocenteGestaoService) { }
+  @Get('/parametros')
+  @ApiOperation({
+    summary: 'Listar parametros',
+  })
+  async findAll(@Query() query: FindParametrosDocenteTO) {
+    return this.service.getTeacherParameters(query);
   }
-
-  @Get()
-  findAll() {
-    return this.docenteGestaoService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.docenteGestaoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDocenteGestaoDto: UpdateDocenteGestaoDto) {
-    return this.docenteGestaoService.update(+id, updateDocenteGestaoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.docenteGestaoService.remove(+id);
+  @Patch('/parametros/:id/toggle')
+  async toggle(@Param('id') id: number) {
+    return this.service.toggleTeacherParameter(Number(id));
   }
 }
--- src/modules/docente_gestao/docente_gestao.service.ts
+++ src/modules/docente_gestao/docente_gestao.service.ts
@@ -2,27 +2,104 @@ import { Injectable } from '@nestjs/common';
 import { CreateDocenteGestaoDto } from './dto/create-docente_gestao.dto';
 import { UpdateDocenteGestaoDto } from './dto/update-docente_gestao.dto';
 import { DataSource } from 'typeorm';
+import { toLowerCaseKeys } from '../util/toLowerCaseKeys';
+import { FindParametrosDocenteTO } from './dto/find-parametros-docente.dto';
 
 @Injectable()
 export class DocenteGestaoService {
-    constructor(private readonly dataSource: DataSource) {}
-  create(createDocenteGestaoDto: CreateDocenteGestaoDto) {
-    return 'This action adds a new docenteGestao';
+  constructor(private readonly dataSource: DataSource) { }
+  async getTeacherParameters(dto: FindParametrosDocenteTO) {
+    const { page = 1, limit = 10, search } = dto
+    const offset = (page - 1) * limit;
+
+    const baseWhere = `
+    ACTIVE_STATE = 1
+    ${search ? `AND (DESIGNACAO LIKE '%${search}%' OR SIGLA LIKE '%${search}%')` : ''}
+  `;
+
+    const sql = `
+    SELECT
+      PK_PARAMETRO      AS codigo,
+      DESIGNACAO        AS designacao,
+      DESCRICAO         AS descricao,
+      SIGLA             AS sigla,
+      ARGS              AS args,
+      OBS               AS observacao,
+      ORDEM             AS ordem,
+      CREATED_AT        AS created_at,
+      UPDATED_AT        AS updated_at
+    FROM FK2_MGD_TB_PARAMETRO_AFECTACAO
+    WHERE ${baseWhere}
+    ORDER BY ORDEM ASC
+    OFFSET ${offset} ROWS FETCH NEXT ${limit} ROWS ONLY
+  `;
+
+    const sqlCount = `
+    SELECT COUNT(*) AS TOTAL
+    FROM FK2_MGD_TB_PARAMETRO_AFECTACAO
+    WHERE ${baseWhere}
+  `;
+
+    const [result, countResult] = await Promise.all([
+      this.dataSource.query(sql),
+      this.dataSource.query(sqlCount),
+    ]);
+
+    const total = Number(countResult[0].TOTAL);
+    const totalPages = Math.ceil(total / limit);
+
+    return {
+      data: await toLowerCaseKeys(result),
+      total,
+      page,
+      limit,
+      totalPages,
+    };
+  }
+  async toggleTeacherParameter(id: number) {
+  const sqlFind = `
+    SELECT PK_PARAMETRO, ARGS
+    FROM FK2_MGD_TB_PARAMETRO_AFECTACAO
+    WHERE PK_PARAMETRO = ${id}
+  `;
+
+  const result = await this.dataSource.query(sqlFind);
+
+  if (!result.length) {
+    throw new Error('Parâmetro não encontrado');
   }
 
-  findAll() {
-    return `This action returns all docenteGestao`;
+  let args;
+
+  try {
+    args = JSON.parse(result[0].ARGS);
+  } catch {
+    throw new Error('ARGS inválido');
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} docenteGestao`;
-  }
+  const currentState = args[0]?.state;
+  const newState = currentState === 'SIM' ? 'NAO' : 'SIM';
 
-  update(id: number, updateDocenteGestaoDto: UpdateDocenteGestaoDto) {
-    return `This action updates a #${id} docenteGestao`;
-  }
+  args[0].state = newState;
 
-  remove(id: number) {
-    return `This action removes a #${id} docenteGestao`;
-  }
+  const updatedArgs = JSON.stringify(args);
+
+  const sqlUpdate = `
+    UPDATE FK2_MGD_TB_PARAMETRO_AFECTACAO
+    SET 
+      ARGS = '${updatedArgs}',
+      UPDATED_AT = SYSDATE
+    WHERE PK_PARAMETRO = ${id}
+  `;
+
+  await this.dataSource.query(sqlUpdate);
+
+  return {
+    message:
+      newState === 'SIM'
+        ? 'Parâmetro activado com sucesso'
+        : 'Parâmetro desactivado com sucesso',
+    state: newState,
+  };
+}
 }
