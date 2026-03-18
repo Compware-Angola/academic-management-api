70fbd75f7df18bc6bbe0342fea6d54ca32a9ea31
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-03-18 09:20:56 +0100

    feat:update data

--- src/modules/defense-management-tfc/defense-management-tfc.module.ts
+++ src/modules/defense-management-tfc/defense-management-tfc.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DefenseManagementTfcService } from './defense-management-tfc.service';
 import { DefenseManagementTfcController } from './defense-management-tfc.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports:[   HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5
+    })],
   controllers: [DefenseManagementTfcController],
   providers: [DefenseManagementTfcService],
 })
