51219674ab85eaa5961cc6981b3de6b185e2be43
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-03-24 16:30:27 +0100

    feat:update data

---
+++ src/modules/assiduidade/dto/FindEstatisticaAssiduidadeDocenteDto.ts
@@ -0,0 +1,73 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString, IsBoolean } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ToBoolean } from 'src/modules/common/decorators/to-boolean.decorator';
+
+
+export class FindEstatisticaAssiduidadeDocenteDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  anoLectivo?: number = 0;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  semestre?: number = 0;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  curso?: number = 0;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  docente?: number = 0;
+
+  @IsOptional()
+  @IsString()
+  dataInicial?: string;
+
+  @IsOptional()
+  @IsString()
+  dataFinal?: string;
+
+  // 🔽 BOOLEANOS LIMPOS
+
+  @ApiProperty({ required: false, default: false })
+  @IsOptional()
+  @ToBoolean()
+  @IsBoolean()
+  naoCobrarFaltas?: boolean = false;
+
+  @ApiProperty({ required: false, default: false })
+  @IsOptional()
+  @ToBoolean()
+  @IsBoolean()
+  exigirPresencasConfirmadas?: boolean = false;
+
+  @ApiProperty({ required: false, default: false })
+  @IsOptional()
+  @ToBoolean()
+  @IsBoolean()
+  exigirSumariosInseridos?: boolean = false;
+
+  @ApiProperty({ required: false, default: false })
+  @IsOptional()
+  @ToBoolean()
+  @IsBoolean()
+  exigirSumariosValidos?: boolean = false;
+
+  // 🔽 PAGINAÇÃO
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  limit?: number = 20;
+}
