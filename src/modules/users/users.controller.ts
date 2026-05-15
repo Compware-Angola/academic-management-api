63351eda9d789cafacfc54251a5866d33f0e3012
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-05-15 17:34:49 +0100

    feat: add user roles enum and additional information endpoint, and update schedule filtering to include docente

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   UseGuards,
   Req,
+  Query,
 } from '@nestjs/common';
 import { TeacherService } from './users.service';
 import { RemoteJwtAuthGuard } from '../common/guard/remote.jwt-auth.guard';
@@ -15,23 +16,43 @@ import { PermissionsGuard } from '../common/secret/permissions.guard';
 
 
 @UseGuards(RemoteJwtAuthGuard, PermissionsGuard)
-@Controller('teacher')
+@Controller('users-ga')
 export class TeacherController {
-  constructor(private readonly teacherService: TeacherService) {}
+  constructor(private readonly teacherService: TeacherService) { }
 
-  @Get('profile/:id')
-  getProfile(@Param('id') id: string ,
-  @Req() req: any
+  @Get('profile')
+  getProfile(
+    @Req() req: any
 
 
-) {
-  const userIdFromToken = req.user?.sub;
-  if (!userIdFromToken) {
-    return {
-      success: false,
-      message: 'User ID not found in token',
-    };
-  }
+  ) {
+    const userIdFromToken = req.user?.sub;
+    if (!userIdFromToken) {
+      return {
+        success: false,
+        message: 'User ID not found in token',
+      };
+    }
     return this.teacherService.profile(userIdFromToken);
+  }
+  @Get('additional-information')
+  getAdditionalInformation(@Req() req: any,
+    @Query('anoLetivo') anoLetivo: number) {
+
+    const userIdFromToken = req.user?.sub;
+    const userRoles = req.user?.roles;
+    if (!userIdFromToken) {
+      return {
+        success: false,
+        message: 'User ID not found in token',
+      };
+    }
+    if (!userRoles) {
+      return {
+        success: false,
+        message: 'User roles not found in token',
+      };
+    }
+    return this.teacherService.getAdditionalInformation(Number(userIdFromToken), userRoles, anoLetivo);
   }
 }
