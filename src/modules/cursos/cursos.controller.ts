c6407d34a8a3314549c0243b4e2ccb03d56b6087
Domingos Canhanga <canhanga96@gmail.com>
2026-05-29 15:11:20 +0100

    refactor: update especialidades query logic to support lookup by matricula code

--- src/modules/cursos/cursos.controller.ts
+++ src/modules/cursos/cursos.controller.ts
@@ -1,24 +1,16 @@
-import {
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CursosService } from './cursos.service';
 import { ApiTags } from '@nestjs/swagger';
-
-
 
 @ApiTags('cursos')
 @Controller('cursos')
 export class CursosController {
-  constructor(
-   private readonly cursosService: CursosService,
-   
-  ) {}
- 
-  @Get('especialidades/:cursoId')
-  especialidadesPorCurso(@Param('cursoId', ParseIntPipe) cursoId: number) {
-    return this.cursosService.buscarEspecialidadesPorCurso(cursoId);
+  constructor(private readonly cursosService: CursosService) {}
+
+  @Get('especialidades/:codigoMatricula')
+  especialidadesPorCurso(
+    @Param('codigoMatricula', ParseIntPipe) codigoMatricula: number,
+  ) {
+    return this.cursosService.buscarEspecialidadesPorMatricula(codigoMatricula);
   }
 }
