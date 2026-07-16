3abb0e2b91600df69cd123a0d878984271da5bd4
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-07-16 14:36:50 +0100

    feat: replace CRUD operations with dashboard statistics report endpoint and implementation

--- src/modules/shared/statistics-reports/statistics-reports.module.ts
+++ src/modules/shared/statistics-reports/statistics-reports.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StatisticsReportsService } from './statistics-reports.service';
 import { StatisticsReportsController } from './statistics-reports.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AcademicYear } from '../entities/academic.year.entity';
+import { AnoLectivoUtil } from 'src/modules/util/current-academic-year';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AcademicYear])],
   controllers: [StatisticsReportsController],
-  providers: [StatisticsReportsService],
+  providers: [StatisticsReportsService, AnoLectivoUtil],
+  exports: [StatisticsReportsService],
 })
-export class StatisticsReportsModule {}
+export class StatisticsReportsModule { }
