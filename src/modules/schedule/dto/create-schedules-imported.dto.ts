6c091038e33d1a571f31c4de50830d6d66b99e1c
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-07-31 14:27:37 +0100

    refactor: update validation decorators and types in CreateImportSchedulesDto and ScheduleImportedDto

--- src/modules/schedule/dto/create-schedules-imported.dto.ts
+++ src/modules/schedule/dto/create-schedules-imported.dto.ts
@@ -1,52 +1,46 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsBoolean, IsNotEmpty, IsNumber } from "class-validator";
-import { Transform, Type } from "class-transformer";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    IsArray,
+    IsBoolean,
+    IsNumber,
+    ValidateNested,
+} from 'class-validator';
+import { Transform, Type } from 'class-transformer';
 
 export class ScheduleImportedDto {
     @ApiProperty({
-        type: Number,
-        description: 'Horários a serem importados',
         example: 1,
     })
-    @IsNotEmpty()
-    @Type(() => Number)
     @IsNumber()
+    @Type(() => Number)
     scheduleId: number;
-
 }
 
 export class CreateImportSchedulesDto {
     @ApiProperty({
-        type: [ScheduleImportedDto],
-        isArray: true,
+        type: () => [ScheduleImportedDto],
     })
-    @IsNotEmpty()
+    @IsArray()
+    @ValidateNested({ each: true })
     @Type(() => ScheduleImportedDto)
     schedulesImported: ScheduleImportedDto[];
-    @IsBoolean()
-    @IsNotEmpty()
+
     @ApiProperty({
-        type: Boolean,
-        description: 'Indica se permite a colisão de aulas ou não',
         example: false,
         default: false,
     })
     @Transform(({ value }) => {
         if (typeof value === 'boolean') return value;
-        if (typeof value === 'string') return value.toLowerCase() === 'true';
+        if (typeof value === 'string') return value === 'true';
         return value;
     })
-    permitiColisao: boolean
+    @IsBoolean()
+    permitiColisao: boolean;
 
     @ApiProperty({
-        type: Number,
-        description: 'Código do ano lectivo de destino',
         example: 2024,
     })
-    @IsNotEmpty()
-    @IsNumber()
     @Type(() => Number)
+    @IsNumber()
     fkanoLectivoDestino: number;
-
-
 }
